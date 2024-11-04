4e1373f32befe594c664200249fc5c3de605dbaf
GunterMagno <bcalejandro@iesrafaelalberti.es>
2024-11-04 10:26:47 +0100

    2.1.10 ~ 2.2.3

--- src/2.1.10.py
+++ src/2.1.10.py
@@ -6,17 +6,27 @@
 #Escribir un programa que pregunte al usuario si quiere una pizza vegetariana o no, y en función de su respuesta le muestre un menú con los ingredientes disponibles para que elija. Solo se puede eligir un ingrediente además de la mozzarella y el tomate que están en todas la pizzas. Al final se debe mostrar por pantalla si la pizza elegida es vegetariana o no y todos los ingredientes que lleva.
 
 
-
-
-
-
-def comprobar_() -> bool:
+def elegir_ingrediente(ingredientes):
     try:
-        str()
+        eleccion = str(input("Elige ingrediente: ")).capitalize
+    except:
+        print("Mal")
+    if eleccion 
+
+
+
+def comprobar_(tipo_pizza) -> bool:
+    try:
+        str(tipo_pizza)
     except ValueError:
         print("*Error*, intenta de nuevo.")
         return False
-    return True
+    if tipo_pizza == "si":
+        return 1
+    elif tipo_pizza == "no":
+        return 2
+    else:
+        return False
 
 
 def limpiar_consola():
@@ -26,9 +36,24 @@ def limpiar_consola():
 
 def main():
     limpiar_consola()
-     = input("")
-    while comprobar_() == False:
-         = input("")
+    ingredientes_vegetarianos = ["Pimiento", "Tofu"]
+    ingredientes_no_vegetarianos = ["Peperoni", "Jamón", "Salmón"]
+    ingredientes_pizza = ["Mozzarella", "Tomate"]
+    tipo_pizza = input("Quieres una pizza vegetariana? (si / no): ").strip().lower()
+    while comprobar_(tipo_pizza) == False:
+        tipo_pizza = input("Quieres una pizza vegetariana? (si / no): ").strip().lower()
+    if comprobar_(tipo_pizza) == 1:
+        print("\nIngredientes disponibles para una pizza vegetariana:\n")
+        for i in ingredientes_vegetarianos:
+            print(f"-> {i}")
+        print("\n")
+        elegir_ingrediente(ingredientes_vegetarianos)
+    elif comprobar_(tipo_pizza) == 2:
+        print("\nIngredientes para una pizza no vegetariana:\n")
+        for i in ingredientes_no_vegetarianos:
+            print(f"-> {i}")
+        print("\n")
+        elegir_ingrediente(ingredientes_no_vegetarianos)
 
 if __name__=="__main__":
     main()
--- src/2.2.3.py
+++ src/2.2.3.py
@@ -1,25 +1,22 @@
+#Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas.
+
+
 def par(numero) -> bool:
     if numero % 2 == 0:
         return True
     elif numero % 2 == 1:
         return False
-
-
-def comprobar_negativo(numero:str) -> str:
-    if numero.startswith("-"):
-        print("El numero es negativo intenta de nuevo: ")
-        return None
-
+    
 
 def comprobar_negativo(numero:str) -> str:
     if numero.startswith("-"):
         numero = numero[:1]
         return numero
-    
+    return numero
 
 def comprobar_numero(numero) -> bool:
     try:
-        float(numero)
+        int(numero)
     except ValueError:
         print("*Error*, intenta de nuevo.")
         return False
@@ -36,8 +33,20 @@ def main():
     numero = input("Dame un numero entero: ")
     while comprobar_numero(numero) == False:
         numero = input("Dame un numero valido: ")
+    numero = comprobar_negativo(numero)
+    numero = int(numero)
+    if par(numero) == True:
+        numero = numero - 1
+        while numero >= 0:
+                cadena = 
+                numero -= 2
+               
+                print(f"{cadena}" ,end=" ")
+    else:
+        while numero >= 0:
+                print(f"{numero}, ", end="")
+                numero -= 2
 
 
 if __name__=="__main__":
     main()
-
